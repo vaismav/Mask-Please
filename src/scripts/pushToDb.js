a9133849d7eafe2de100b82020ca7396f8ecb3fc
Avishai Vaisman <aviciiv@gmail.com>
2022-03-14 00:22:23 +0200

    cleanups

--- src/scripts/pushToDb.js
+++ src/scripts/pushToDb.js
@@ -1,51 +1,6 @@
-import {db, newDoc,updateMeta} from '../CloudAPI/FireBase';
+import {newDoc,updateMeta} from '../CloudAPI/FireBase';
 import data from './allData';
 
-// type dbDoc = {
-//     identifier: string,
-//     description: string,
-//     justification: string[],
-//     justificationClustered: string[],
-//     salutation: string[],
-//     closing: string[],
-//     selfReference: string[],
-//     image: string[],
-//     language: string[],
-//     format: string[],
-//     script: string[],
-//     material: string[],
-//     businessType: string[],
-//     title: string,
-//     date: string,
-//     latitude: number,
-//     longitude: number,
-//     spatialCoverage: string[],
-//     creator:string,
-//     comment: string
-// };
-
-// type entryData = {
-//     identifier: string,
-//     description: string,
-//     justification: string,
-//     justificationClustered: string,
-//     salutation: string,
-//     closing: string,
-//     selfReference: string,
-//     image: string,
-//     language: string,
-//     format: string,
-//     script: string,
-//     material: string,
-//     businessType: string,
-//     title: string,
-//     date: string,
-//     latitude: string,
-//     longitude: string,
-//     spatialCoverage: string,
-//     creator:string,
-//     comment: string
-// };
 
 const emptyDbDoc = {
     identifier: "",
@@ -97,53 +52,6 @@ const Json2DbDoc = (data) =>{
 
     return dbDoc;
 }
-
-const dataTest = [
-    {
-        identifier: "20200621_084340.jpg",
-        "description": "הכניסה מותרת בחבישת מסכה בלבד!!",
-        "justification": "",
-        "justificationClustered": "",
-        "salutation": "",
-        "closing": "",
-        "selfReference": "",
-        "image": "smilly|heart",
-        "language": "Hebrew|English",
-        "format": "hand written|printed",
-        "script": "cursive",
-        "material": "paper|pen",
-        "businessType": "",
-        "title": "2020-06-21 - תל אביב - יפו, הצפון החדש - סביבת ככר המדינה",
-        "date": "44003",
-        "latitude": "32.0893843",
-        "longitude": "34.7826084",
-        "spatialCoverage": "148, אבן גבירול, תל אביב - יפו, הצפון החדש - סביבת ככר המדינה, תל אביב-יפו, מחוז תל אביב, ישראל",
-        "creator": "Yael Netzer",
-        "comment": "HAHAHA comment"
-    },
-    {
-        "identifier": "20200704_174832.jpg",
-        "description": "שלום אורחים נכבדים של כלבו אבישי, נא להכנס עם מסיכה בלבד יש לשמור על 2 מטר מרחק אין להכנס בהתקהלות אלא לפי כללי הזהירות נא לשמור על ההנחיות למען ביטחונכם וביטחון הציבור. תודה.",
-        "justification": "למען ביטחונכם|ביטחון הציבור",
-        "justificationClustered": "למען ביטחונכם|ביטחון הציבור",
-        "salutation": "אורחים נכבדים",
-        "closing": "תודה",
-        "selfReference": "כלבו אבישי",
-        "image": "",
-        "language": "Hebrew",
-        "format": "printed",
-        "script": "square",
-        "material": "paper|printer",
-        "businessType": "חלפן כספים",
-        "title": "2020-07-04 - תל אביב - יפו, פלורנטין",
-        "date": "44016",
-        "latitude": "32.0612607",
-        "longitude": "34.7736749",
-        "spatialCoverage": "בית שבתאי-מויאל, אלנבי, תל אביב - יפו, פלורנטין, תל אביב-יפו, מחוז תל אביב, ישראל",
-        "creator": "Yael Netzer",
-        "comment": "",
-    }
-];
 
 const update = () => {
 
